376902d2adbc525ec150b8224a5860c7a0965c20
Ian <ronesp.85@gmail.com>
2023-04-06 13:59:08 -0700

    push to home

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,7 +10,7 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15}
         groundColor="black"/>
-        <pintLight intensity={1} />
+        <pointLight intensity={1} />
         <primitive
           object={computer.scene}
         />
@@ -23,9 +23,18 @@ const ComputersCanvas = () => {
     <Canvas
       framelop="demand"
       shadows
-      camera=
+      camera={{ position: [20,3,5], fov: 25 }}
+      gL={{ preserveDrawingBuffer: true }}
     >
-
+      <Suspense fallback={<CanvasLoader />} >
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload />
     </Canvas>
   )
 }
